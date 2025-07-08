7dd9771a28c3e95242ea0e3555b13217c71208f5
Davano <132116963+kakadavano@users.noreply.github.com>
2025-07-08 20:49:12 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const button = document.querySelector("button");
+const popup = document.querySelector(".popup");
 
 button.addEventListener ("click", (e) => {
     button.classList.toggle("hidden");
+    popup.classList.toggle("active");
 });
